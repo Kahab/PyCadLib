79eb5201c46586dac251393509b4cad93562ea09
kahab <kaihaberland@gmx.net>
2022-09-13 17:30:33 +0200

    Added creation of test table and test insert

--- database_handler.py
+++ database_handler.py
@@ -1,3 +1,4 @@
+#from msilib.schema import tables
 import sqlite3
 
 def create_db(db_name, path):
@@ -9,15 +10,39 @@ def connect2db(db_file):
     try:
         conn = sqlite3.connect(db_file)
         print(sqlite3.version)
+        return conn
     except Error as e:
         print(e)
-    finally:
-        if conn:
-            conn.close()
+        return None
 
 
 if __name__ == '__main__':
     db_file = 'database/cad_lib.db'
-    connect2db(db_file)
+    conn = connect2db(db_file)
     
+    table_creation_string = """CREATE TABLE Test (
+        TestID int,
+        TestVar1 varchar(255),
+        TestVar2 varchar(255)
+        );"""
+        
+    table_insert_string = """INSERT INTO Test VALUES (1, 'test1', 'test2');"""
     
+    if conn:
+        #conn.execute(table_creation_string)
+        #conn.execute(table_insert_string)
+        #conn.execute('SELECT * from Test;')
+    
+        cur = conn.cursor()
+        
+        #cur.execute(table_insert_string)
+        #conn.commit()
+        cur.execute("SELECT * FROM Test")
+
+        rows = cur.fetchall()
+
+        for row in rows:
+            print(row)
+    
+    else:
+        print('No conn')
