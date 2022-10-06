f62a699d871fecba0f096e2bcd73c0c4cc989243
Kai Haberland <kai.haberland2@gmail.com>
2022-10-06 20:51:39 +0200

    First window initialization.

---
+++ visu_main.py
@@ -0,0 +1,13 @@
+from PyQt6.QtWidgets import QApplication, QWidget
+import sys
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    window = QWidget()
+    window.show()
+
+    app.exec()
+
+
